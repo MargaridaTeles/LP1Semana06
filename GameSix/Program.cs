5345c6580dce6349e9f5f369184265cea27b5d7d
MargaridaTeles <telesmaegarida@gmail.com>
2023-04-20 19:01:12 +0100

    Pedir ao utilizador nome, criar objeto Foe e guardar no array

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -10,6 +10,13 @@ namespace GameSix
             int num_inimigos = int.Parse(Console.ReadLine());
             string[] array_inimigos = new string[num_inimigos];
 
+            for (int i=0; i < array_inimigos.Length; i++)
+            {
+                Console.Write("Insira um Nome: ");
+                string nome_inimigo = Console.ReadLine();
+                array_inimigos[i] = nome_inimigo;
+                Foe inimigo = new Foe(nome_inimigo);
+            }
 
         }
     }
