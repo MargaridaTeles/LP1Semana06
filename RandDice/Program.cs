9290d13d4e16f1a6787d1cfecc5d871856056ef8
MargaridaTeles <telesmaegarida@gmail.com>
2026-03-26 10:29:00 +0000

    Add a random (1, 7) to randomize the number for the dice

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -6,7 +6,20 @@ namespace RandDice
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            int n = int.Parse(args[0]);
+            int s = int.Parse(args[1]);
+
+            Random rnd = new Random(s);
+
+            int soma = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int valor = rnd.Next(1, 7);
+                soma += valor;   
+            }
+            Console.WriteLine($"{soma}");
+
         }
     }
 }
