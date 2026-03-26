d4305279ba8ac05a5ae7365ae9cce55543e61f92
MargaridaTeles <telesmaegarida@gmail.com>
2026-03-26 10:54:51 +0000

    Add inputs for user and print the results

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,21 @@ namespace MyGame
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            int numEnemys = int.Parse(args[0]);
+            Enemy[] enemies = new Enemy[numEnemys];
+
+            for(int i = 0; i < numEnemys; i++)
+            {
+                Console.Write($"Nome do inimigo {i + 1}: ");
+                string name = Console.ReadLine();
+
+                enemies[i] = new Enemy(name);
+            }
+
+            for (int j = 0; j < numEnemys; j++)
+            {
+                Console.WriteLine($"{enemies[j].GetName()} {enemies[j].GetHealth()} {enemies[j].GetShield()}");
+            }
         }
     }
 }
